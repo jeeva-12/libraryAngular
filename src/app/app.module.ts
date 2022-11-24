a44a9bd19bfa266c6455385007238a61898450ff
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-11-24 19:18:20 +0530

    admlogin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdministatorLoginComponent } from './administator-login/administator-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdministatorLoginComponent
   ],
   imports: [
     BrowserModule,

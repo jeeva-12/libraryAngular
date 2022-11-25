dc9d36fce0a18e11aae96dff9fa01ffe18d06147
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-11-25 11:40:55 +0530

    hgf

--- src/app/book-view/book-view.component.ts
+++ src/app/book-view/book-view.component.ts
@@ -6,5 +6,96 @@ import { Component } from '@angular/core';
   styleUrls: ['./book-view.component.css']
 })
 export class BookViewComponent {
+  data:any= [
+    {
+      "image":"https://www.hookedtobooks.com/wp-content/uploads/2020/10/Best-Books-By-James-Patterson-533x800.png",
+      "name":"Great of Young",
+      "author":"James",
+      "Language":"English",
+      "Price":769
+
+    },
+    {
+      "image":"https://www.washingtonpost.com/rf/image_1484w/2010-2019/WashingtonPost/2017/03/08/BookWorld/Images/Patterson_THEBLACKBOOK.JPG?t=20170517",
+      "name":"Blackbook",
+      "author":"James",
+      "Language":"English",
+      "Price":769
+
+    },
+    {
+      "image":"https://i.pinimg.com/originals/f3/4a/fb/f34afba4c96693766a05b09cb4e5b689.jpg" ,
+      "name":"First lady",
+      "author":"James",
+      "Language":"English",
+      "Price":769
+
+    },
+    {
+      "image":"https://i.pinimg.com/originals/f3/4a/fb/f34afba4c96693766a05b09cb4e5b689.jpg" ,
+      "name":"Great of Young",
+      "author":"James",
+      "Language":"English",
+      "Price":769
+
+    },
+    {
+      "image":"https://www.adazing.com/wp-content/uploads/2020/04/james-68-857x1536.jpg",
+      "name":"Moorse",
+      "author":"James",
+      "Language":"English",
+      "Price":769
+
+    },
+    {
+      "image":"https://www.hachettebookgroup.com/wp-content/uploads/2021/12/9780316397636.jpg?fit=450%2C675",
+      "name":"Stories",
+      "author":"James",
+      "Language":"English",
+      "Price":769
+
+    },
+    {
+      "image":"https://images-na.ssl-images-amazon.com/images/I/61r-lZz7t7L.jpg",
+      "name":"Great of Young",
+      "author":"15 Affia",
+      "Language":"English",
+      "Price":469
+
+    },
+    {
+      "image":"https://i.pinimg.com/originals/f4/c4/0f/f4c40fae470793402725723ab1c12395.jpg",
+      "name":"Mistress",
+      "author":"James",
+      "Language":"English",
+      "Price":769
+
+    },
+    {
+      "image":"https://images-na.ssl-images-amazon.com/images/I/71pN%2BMQIv4L.jpg",
+      "name":"Great of Young",
+      "author":"James",
+      "Language":"English",
+      "Price":769
+
+    },
+    {
+      "image":"https://images-na.ssl-images-amazon.com/images/I/91VlbHPfvsL.jpg",
+      "name":"Wind",
+      "author":"James",
+      "Language":"English",
+      "Price":864
+
+    },
+    {
+      "image":"https://cdn2.penguin.com.au/covers/original/9780143787495.jpg",
+      "name":"liar",
+      "author":"James",
+      "Language":"English",
+      "Price":643
+
+    }
+
+  ]
 
 }

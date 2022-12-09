f9188aaaf9aa5ef30e653c5cc12be086cc3a7391
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-12-09 07:53:02 +0530

    sd

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-login',
@@ -10,17 +11,23 @@ export class UserLoginComponent {
   username=""
   password=""
 
-  logIn=()=>
-  {
-    let data:any=
-    {
-      username:this.username,
-  password:this.password
+  constructor(private router:Router) { }
+
+  readValue = () => {
+    let data: any = {
+      "username": this.username, 
+      "password": this.password
     }
     console.log(data)
 
-
+    if (this.username == "user" && this.password == "12345") {
+      this.router.navigate(['/view'])
+    }
+    else {
+      alert("invalid credentials")
+    }
+  }
     
   }
 
-}
+
--- src/app/user-registrartion/user-registrartion.component.ts
+++ src/app/user-registrartion/user-registrartion.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-user-registrartion',
@@ -8,33 +9,59 @@ import { Component } from '@angular/core';
 export class UserRegistrartionComponent {
 
   name=""
-  adhar=""
   address=""
-  pin=""
+  pincode=""
+  aadharnumber=""
   dob=""
   email=""
-  mobile=""
+  phonenumber=""
   username=""
   password=""
-  conpassword=""
+  confirmpassword=""
 
-  regist=()=>
-  {
-    let data:any=
-    {
-      name:this.name,
-  adhar:this.adhar,
-  address:this.address,
-  pin:this.pin,
-  dob:this.dob,
-  email:this.email,
-  mobile:this.mobile,
-  username:this.username,
-  password:this.password,
-  conpassword:this.conpassword
-    }
-    console.log(data)
+  constructor(private api:ApiService){}
+                    
+userRegister=()=>
+{
+  let data:any={
+    
+    "name":this.name,
+    "address":this.address,
+    "pincode":this.pincode,   
+    "aadharnumber":this.aadharnumber,
+    "dob":this.dob,
+    "email":this.email,
+    "phonenumber":this.phonenumber,
+    "username":this.username,   
+    "password":this.password,
+    "confirmpassword":this.confirmpassword
+
   }
+
+  console.log(data)
+
+  this.api.adduser(data).subscribe(
+    (response:any)=>{
+      console.log(response)
+      if (response.status=="success"){
+        this.name=""
+        this.address=""
+        this.pincode=""
+        this.aadharnumber=""
+        this.dob=""
+        this.email=""
+        this.phonenumber=""
+        this.username=""
+        this.password=""
+        this.confirmpassword=""
+      } else{
+        alert("something went wrong")
+
+      }
+      
+    }
+  )
+}
 
 
 
